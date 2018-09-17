65a7ddff1e92c947ed3fed18918da2954191d5fe
Martin Griffiths <martin.griffiths@ofsted.gov.uk>
2018-09-17 14:40:43 +0100

    added several plots

--- knizia.R
+++ knizia.R
@@ -5,6 +5,7 @@ library(purrr)
 library(readr)
 library(ggplot2)
 library(plotly)
+library(forcats)
 
 get_ids <- function(page_num){
   games_page <- paste0("https://boardgamegeek.com/search/boardgame/page/",
@@ -69,30 +70,56 @@ game_df <- read_csv('knizia_full.csv')
 game_df <- game_df %>% 
   filter(yearpublished >= 1990, `ranks/rank` != 'Not Ranked')
 
+
+
+game_df <- game_df %>%
+  mutate(weightcat = cut(averageweight, 
+                         breaks = c(0.9, 2, 3, 4),
+                         labels = c('1.0-2.0','2.0-3.0','3.0 +')),
+         ratingcat = cut(average, 
+                         breaks = c(4, 6, 7, 9),
+                         labels = c('less than 6','6 - 7','over 7')),
+         timecat = cut(playingtime,
+                       breaks = c(1,30.5,60.5,180),
+                       labels = c('<= 30 mins', '<= 60 mins','> 60 mins')))
+
 gg <- game_df %>%
   ggplot(aes(x=yearpublished, y=average, 
-             size = usersrated, text = name)) +
-  geom_point(alpha = 0.5, color = 'blue') +
+             size = usersrated, text = name,
+             color = weightcat)) +
+  geom_point(alpha = 0.5) +
   theme_minimal() +
   labs(y = "Average user rating",
        x = "Year published",
-       title = "BGG-ranked Knizia games")
+       title = "BGG-ranked Knizia games") +
+  scale_size_continuous('Users rating', breaks= c(100, 1000, 10000)) +
+  scale_colour_discrete('Weight')
 
-ggplotly(gg, tooltip = 'text')
-
-game_df %>%
-  filter(average > 7.0) %>%
-  ggplot(aes(x=yearpublished)) +
-  geom_bar()
+ggplotly(gg, tooltip = 'text') 
 
 game_df %>%
-  filter(usersrated > 5000) %>%
-  ggplot(aes(x=yearpublished)) +
-  geom_bar()
+  ggplot(aes(x=yearpublished, fill= weightcat)) +
+  geom_bar(width = 0.9)
+
+game_df %>%
+  ggplot(aes(x=yearpublished, fill= fct_rev(ratingcat))) +
+  geom_bar(width = 0.9, position = 'fill') +
+  labs(x = 'Year published', y='', title = 'BGG-ranked Knizia games') +
+  scale_fill_discrete('User rating') +
+  theme_minimal()
+
+game_df %>%
+  ggplot(aes(x=yearpublished, fill= fct_rev(timecat))) +
+  geom_bar(width = 0.9, position = 'fill') +
+  labs(x = 'Year published', y='', title = 'BGG-ranked Knizia games') +
+  scale_fill_discrete('User rating') +
+  theme_minimal()
 
 gg <- game_df %>%
   filter(averageweight > 0.5, numweights > 5) %>%
   ggplot(aes(y=average, x=averageweight, text=name)) +
   geom_point() +
-  geom_smooth()
+  labs(x = 'Weight', y='User rating') +
+  theme_minimal()
 
+ggplotly(gg, tooltip = 'text')
